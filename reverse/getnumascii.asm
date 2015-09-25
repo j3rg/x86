f9e4254013265edc35f53b8bdd7748299b4ba854
dr-z3r0 <key.rookie@gmail.com>
2015-09-25 00:17:59 -0600

    working getnumascii.asm

--- reverse/getnumascii.asm
+++ reverse/getnumascii.asm
@@ -11,9 +11,14 @@ _start:
   ;mov edx,TXTLEN  ; specify length of string
   ;int 0x80        ; make kernel sys_call
 
-  mov eax,INNUM   ; copy hex number to EAX
+  mov eax, [INNUM]  ; copy hex number EAX
   cmp eax,0x03E8   ; check if number is greater than 1ooo
-  jge ErrMsg      ; give error and exit
+  jge ErrMsg       ; give error and exit
+
+  mov dword [ASCIINUM],0x0A303030
+  ;mov [ASCINUM+1],0x30
+  ;mov [ASCINUM+2],0x30
+  ;mov [ASCINUM+3],0x0A
 
   cmp eax,0x0A    ; compare to 1
   jl  find_1      ; find one
@@ -25,12 +30,12 @@ _start:
 find_100:
 
   sub eax,0x64    ; subtract 100 from number
-  add ebx,1       ; add one to EBX
+  add ebx,0x01       ; add one to EBX
   cmp eax,0x64    ; compare to 100
   jge find_100      ; jump to find digit in tens decimal place
 
   mov ecx,[NUM+ebx]
-  mov [ASCIINUM],ecx
+  mov [ASCIINUM],cl
 
   cmp eax,0x0A
   jl find_1
@@ -40,42 +45,42 @@ find_100:
 find_10:
 
   sub eax,0x0A    ; subtract 100 from number
-  add ebx,1       ; add one to EBX
+  add ebx,0x01       ; add one to EBX
   cmp eax,0x0A    ; compare to 100
   jge find_10     ; jump to find digit in tens decimal place
 
   mov ecx,[NUM+ebx]
-  mov [ASCIINUM+1],ecx
+  mov [ASCIINUM+1],cl
 
 find_1:
 
   mov ecx,[NUM+eax]
-  mov [ASCIINUM+2],ecx
+  mov [ASCIINUM+2],cl
 
-  mov eax,4       ; sys_write
-  mov ebx,1       ; file descriptor
-  mov ecx,TXTIN   ; file pointer
-  mov edx,TXTLEN  ; length of string
+  mov eax,0x04       ; sys_write
+  mov ebx,0x01       ; file descriptor
+  mov ecx,ASCIINUM   ; file pointer
+  mov edx,0x04  ; length of string
   int 0x80        ; make kernel sys_call
 
   jmp Done
 
 ErrMsg:
-  mov eax,4       ; sys_write
-  mov ebx,1       ; file descriptor
+  mov eax,0x04       ; sys_write
+  mov ebx,0x01       ; file descriptor
   mov ecx,ErrBigNum ; file pointer
   mov edx,ErrBigLen ; length of string
   int 0x80          ; make kernel sys_call
 
 Done:
-  mov eax,1       ; sys_exit
-  mov ebx,0       ; return code
+  mov eax,0x01       ; sys_exit
+  mov ebx,0x00       ; return code
   int 0x80        ; make kernel sys_call
 
 [SECTION .data]
-  ErrBigNum  db "Number is greater than or equals to 1000",10
-  ErrBigLen equ:$-ErrBigNum
-  NUM db 30,31,32,33,34,35,36,37,38,39
-  INNUM db 3C       ; input number
+  ErrBigNum:  db "Number is greater than or equals to 1000",10
+  ErrBigLen: equ $-ErrBigNum
+  NUM: db 0x30,0x31,0x32,0x33,0x34,0x35,0x36,0x37,0x38,0x39
+  INNUM db 0x3C       ; input number
 [SECTION .bss]
-  ASCIINUM resb 3   ; reserve bytes for input
+  ASCIINUM resb 4   ; reserve bytes for input
